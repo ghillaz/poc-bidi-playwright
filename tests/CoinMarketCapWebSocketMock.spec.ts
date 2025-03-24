32800fec01faded45dc971297b8809b088e0b19f
ghillaz <santos.gue@gmail.com>
2025-03-24 14:21:11 +0000

    fix: correct flakyness

--- tests/CoinMarketCapWebSocketMock.spec.ts
+++ tests/CoinMarketCapWebSocketMock.spec.ts
@@ -49,6 +49,14 @@ const mockResponse15s = {
 };
 
 test.beforeEach(async ({ page }) => {
+
+});
+
+test("Mock WebSocket messages and verify price update", async ({ page }) => {
+  const coinMarketCapPage = new CoinMarketCapPage(page);
+
+  // Note that only `WebSocket`s created after this method ('routeWebSocket') was called will be routed.
+  // It is recommended to call this method before navigating the page.
   await page.routeWebSocket(`${process.env.WEB_SOCKET_URL}`, (ws) => {
     ws.onMessage((message) => {
       const messageStr = message.toString();
@@ -68,13 +76,7 @@ test.beforeEach(async ({ page }) => {
       }
     });
   });
-});
 
-test("Mock WebSocket messages and verify price update", async ({ page }) => {
-  const coinMarketCapPage = new CoinMarketCapPage(page);
-
-  // Note that only `WebSocket`s created after this method was called will be routed. It is recommended to call this
-  // method before navigating the page.
   await page.goto(`${process.env.BTC_URL}`);
 
   // Wait for the page to load -> Flaky Test
